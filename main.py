b7b84e7f94f2b268903926b70c065be46236ba45
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-07 17:41:40 +0600

    Turtle moves clockwise

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ def move_backwards():
 def counter_clockwise():
     tim.left(5)
 def clockwise():
-    pass
+    tim.right(5)
 
 
 
 
 screen.listen() #starts listening
-screen.onkey(key="w", fun=move_forwards) #calls the move forward function when spacebar is hit
-screen.onkey(key="s", fun=move_backwards) #calls the move forward function when spacebar is hit
-screen.onkey(key="a", fun=counter_clockwise) #calls the move forward function when spacebar is hit
-
+screen.onkey(key="w", fun=move_forwards) #calls the move_forward function when w is hit
+screen.onkey(key="s", fun=move_backwards) #calls the move_backwards function when s is hit
+screen.onkey(key="a", fun=counter_clockwise) #calls the counter_clockwise function when a is hit
+screen.onkey(key="d", fun=clockwise) #calls the clockwise function when d is hit
 
 
 
