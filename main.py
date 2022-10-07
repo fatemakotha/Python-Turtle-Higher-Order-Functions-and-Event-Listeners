470658359036e122583aa11a89feb612802878f3
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-07 17:18:13 +0600

    Used screen.listen() and screen.onkey()

--- main.py
+++ main.py
@@ -5,4 +5,18 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 
+def move_forwards():
+    tim.forward(10)
 
+
+
+
+screen.listen() #starts listening
+screen.onkey(key="space", fun=move_forwards) #calls the move forward function when spacebar is hit
+
+
+
+
+
+
+screen.exitonclick()
