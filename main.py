c62697c75f6cd6d85a773e18ac481504644109be
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-07 23:02:09 +0600

    Used clear() to clear turtle's drawing

--- main.py
+++ main.py
@@ -17,13 +17,18 @@ def turn_right():
     new_heading = tim.heading() - 10
     tim.setheading(new_heading)
 
+def clear_screen():
+    tim.clear()
+
+
 
 
 screen.listen() #starts listening
-screen.onkey(key="w", fun=move_forwards) #calls the move_forward function when w is hit
-screen.onkey(key="s", fun=move_backwards) #calls the move_backwards function when s is hit
-screen.onkey(key="a", fun=turn_left) #calls the counter_clockwise function when a is hit
-screen.onkey(key="d", fun=turn_right) #calls the clockwise function when d is hit
+screen.onkey(key="w", fun=move_forwards) #calls the move_forward function when w is hit **
+screen.onkey(key="s", fun=move_backwards) #calls the move_backwards function when s is hit **
+screen.onkey(key="a", fun=turn_left) #calls the counter_clockwise function when a is hit **
+screen.onkey(key="d", fun=turn_right) #calls the clockwise function when d is hit **
+screen.onkey(key="c", fun=clear_screen) #calls the clear_screen function when c is hit **
 
 
 
